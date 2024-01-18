64ee12548ac31cba32c77a10f346cd8c92606726
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-01-18 14:19:07 +0100

    Login Logic
    Implement Login With Google Functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const GoogleStrategy = require('passport-google-oidc');
 const crypto = require('crypto')
 const db = require('../db')
 const router = express.Router()
@@ -19,6 +21,52 @@ passport.use(new LocalStrategy(function verify(username, password, cb){
     })
 }))
 
+passport.use(new GoogleStrategy({ 
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/oauth2/redirect/google',
+    scope: [ 'profile' ]
+}, function verify(issuer, profile, cb) {
+
+    console.log('Profile : ', profile)
+    console.log('Issuer : ', issuer)
+
+    db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+      issuer,
+      profile.id
+    ], function(err, row) {
+      if (err) { return cb(err); }
+      if (!row) {
+        db.run('INSERT INTO users (username) VALUES (?)', [
+          profile.displayName
+        ], function(err) {
+          if (err) { return cb(err); }
+  
+          var id = this.lastID;
+          db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+            id,
+            issuer,
+            profile.id
+          ], function(err) {
+            if (err) { return cb(err); }
+            var user = {
+              id: id,
+              name: profile.displayName
+            };
+            return cb(null, user);
+          });
+        });
+      } else {
+        db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
+          if (err) { return cb(err); }
+          if (!row) { return cb(null, false); }
+          console.log(row);
+          return cb(null, row);
+        });
+      }
+    });
+  }));
+
 passport.serializeUser(function(user, cb){
     process.nextTick(function(){
         cb(null, {id: user.id, username: user.username})
@@ -29,6 +77,13 @@ passport.deserializeUser(function(user, cb){
         return cb(null, user)
     })
 })
+
+router.get('/login/federated/google', passport.authenticate('google'))
+
+router.get('/oauth2/redirect/google', passport.authenticate('google', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}));
 
 router.post('/login/password', passport.authenticate('local', {
     successRedirect: '/',
